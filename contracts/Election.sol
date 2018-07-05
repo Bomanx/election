f2845e6d4cefd501b76efafda9f345a6593cae0d
Bomanx <boman@bu.edu>
2018-07-06 01:17:45 +0800

    Initial commit

---
+++ contracts/Election.sol
@@ -0,0 +1,11 @@
+pragma solidity ^0.4.24;
+
+/**
+ * The contractName contract does this and that...
+ */
+contract Election {
+	string public candidate;
+	function Election () public{
+		candidate = "Candidate 1";
+	}
+}
